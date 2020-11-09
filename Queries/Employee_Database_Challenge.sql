61957f46a59f3e0771aefb5538cc5b8b6e1281df
John Bates <jbatesy@yahoo.com>
2020-11-08 20:56:36 -0500

    2nd update to challenge

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,100 @@
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dep_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dep_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dep_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dep_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+Select * FROM departments;
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO sales_dept_info
+FROM current_emp as ce
+INNER JOIN dep_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_no = 'd007';
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO sales_development_dept_info
+FROM current_emp as ce
+INNER JOIN dep_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_no IN ('d007', 'd005');
+
+-- ==================================Start Challenge Code========================================================
+
 SELECT e.emp_no,
 e.first_name,
 e.last_name,
@@ -12,7 +109,7 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;    
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (r.emp_no, r.to_date)
+SELECT DISTINCT ON (r.emp_no)
 r.emp_no,
 r.first_name,
 r.last_name,
@@ -21,12 +118,15 @@ INTO unique_titles
 FROM retirement_titles as r
 ORDER BY r.emp_no, r.to_date DESC;
 
+SELECT * FROM unique_titles;
+
 --  Employee count by title
 SELECT COUNT(ut.emp_no), ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT DESC;
+
 -- ======================Challenge 2================================================
 
 SELECT DISTINCT ON (e.emp_no)
